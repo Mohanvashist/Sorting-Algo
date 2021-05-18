7e74749db842c321375f943622b19c122497d659
mohan <mohanvashist001@gmail.com>
2021-05-18 18:33:33 +0530

    linear search

--- src/main/searching/LinearSearch.java
+++ src/main/searching/LinearSearch.java
@@ -1,4 +1,21 @@
 package main.searching;
 
 public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {12,15,75,48,65,95,23,45,85,1,125,235,6};
+
+        System.out.println("Index : "+linearSearch(arr,1111));
+    }
+
+    public static int linearSearch(int[] arr, int x){
+
+        int index = -1;
+        for(int i=0;i<arr.length-1;i++){
+            if(x==arr[i]) {
+                index=i;
+            }
+        }
+        return index;
+
+    }
 }
