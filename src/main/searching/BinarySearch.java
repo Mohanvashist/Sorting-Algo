6bc4a984f7e9b14324d358d0018eb80cad8aacd5
mohan <mohanvashist001@gmail.com>
2021-05-18 19:18:27 +0530

    binary search

--- src/main/searching/BinarySearch.java
+++ src/main/searching/BinarySearch.java
@@ -1,4 +1,27 @@
 package main.searching;
 
 public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+
+        System.out.println("Index : "+binarySearch(arr,0,arr.length-1,9));
+    }
+
+    public static int binarySearch(int[] arr,int l, int r, int x){
+
+        if(r>=1){
+            int mid = l +(r-l)/2;
+
+            if(arr[mid]==x) {
+                return mid;
+            }
+
+            if(x>arr[mid])
+                return binarySearch(arr,mid+1,r,x);
+
+            if(x<arr[mid])
+                return binarySearch(arr,l,mid-1,x);
+        }
+        return -1;
+    }
 }
