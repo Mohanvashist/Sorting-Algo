7f61ca80781e851cf83bd91c9f78fa6d115a7c9e
mohan <mohanvashist001@gmail.com>
2021-05-18 14:26:09 +0530

    First commit

--- src/main/sorting/BubbleSort.java
+++ src/main/sorting/BubbleSort.java
@@ -4,6 +4,20 @@ public class BubbleSort {
 
     public static void doSort(int[] arr){
 
+        for(int i=0;i<arr.length-1;i++){
+            boolean swapped= false;
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if(!swapped)
+                break;
+        }
     }
 
     public static void main(String[] as){
