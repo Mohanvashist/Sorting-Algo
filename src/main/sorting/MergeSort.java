02af0d0a5d6cbcf4caa83fbde65b34a376a3cdc6
mohan <mohanvashist001@gmail.com>
2021-05-18 15:00:19 +0530

    Merge sort implemented

--- src/main/sorting/MergeSort.java
+++ src/main/sorting/MergeSort.java
@@ -1,4 +1,77 @@
 package main.sorting;
 
 public class MergeSort {
+
+    public static void doSort(int[] arr, int l, int r){
+        if(l<r){
+            int m = (l+r)/2;
+
+            doSort(arr,l,m);
+            doSort(arr,m+1,r);
+
+            merge(arr,l,m,r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12,15,9,78,55,96,63,45,122,32,88,7,11,56};
+        System.out.println("Unsorted: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+", ");
+        }
+        doSort(arr, 0, arr.length-1);
+        System.out.println("\nSorted: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+", ");
+        }
+    }
 }
